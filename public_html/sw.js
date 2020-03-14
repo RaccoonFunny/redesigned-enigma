965841bd983bee944c1d6ff877e62c0869c7b7bf
RaccoonFunny <62151481+RaccoonFunny@users.noreply.github.com>
2020-03-15 02:30:12 +0300

    Update sw.js

--- public_html/sw.js
+++ public_html/sw.js
@@ -5,7 +5,8 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE).then((cache) => {
             return cache.addAll([
-                '/img/background'
+                '/img/favourite.png',
+                '/index.html'
             ]);
         })
     );
